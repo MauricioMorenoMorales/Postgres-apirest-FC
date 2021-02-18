aa588ac3a3d73eea8c032b79df2468ff2ebd9e6e
mauriciomoreno <mauriciomorenomorales@protonmail.ch>
2021-02-18 07:49:06 -0600

    Completed

--- src/controllers/index.controller.js
+++ src/controllers/index.controller.js
@@ -13,6 +13,49 @@ const getUsers = async (req, res) => {
 	res.status(200).json(response.rows)
 }
 
+const getUserById = async (req, res) => {
+	const { id } = req.params
+	const response = await pool.query('SELECT * FROM users WHERE id = $1', [id])
+	res.json(response.rows)
+}
+
+const createUser = async (req, res) => {
+	const { name, email } = req.body
+	const response = await pool.query(
+		'INSERT INTO users (name, email) VALUES($1, $2)',
+		[name, email],
+	)
+	console.log(response)
+	res.json({
+		message: 'User Added Succesfully',
+		body: {
+			user: { name, email },
+		},
+	})
+}
+
+const updateUser = async (req, res) => {
+	const { id } = req.params
+	const { name, email } = req.body
+	const response = await pool.query(
+		'UPDATE users SET name = $1, email = $2 WHERE id = $3',
+		[name, email, id],
+	)
+	console.log(response)
+	res.json('User pudate succesfully')
+}
+
+const deleteUser = async (req, res) => {
+	const { id } = req.params
+	const response = await pool.query('DELETE FROM users WHERE id = $1', [id])
+	console.log(response)
+	res.json(`User ${id} deleted successfully`)
+}
+
 module.exports = {
 	getUsers,
+	createUser,
+	getUserById,
+	deleteUser,
+	updateUser,
 }
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -4,5 +4,9 @@ const router = Router()
 const userController = require('../controllers/index.controller')
 
 router.get('/', userController.getUsers)
+router.get('/:id', userController.getUserById)
+router.post('/', userController.createUser)
+router.delete('/:id', userController.deleteUser)
+router.put('/:id', userController.updateUser)
 
 module.exports = router
